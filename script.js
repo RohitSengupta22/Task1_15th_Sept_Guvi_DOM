4292c38e3d6090953d9d4fff4894e711a4739a1e
RohitSengupta22 <chints.rsg@gmail.com>
2023-09-19 11:23:28 +0530

    changes made

--- script.js
+++ script.js
@@ -24,3 +24,34 @@ function substract() {
 function calculate(){
     document.getElementById("input").value = eval( document.getElementById("input").value)
 }
+
+function one(){
+    document.getElementById("input").value =  document.getElementById("input").value + "1"
+}
+function two(){
+    document.getElementById("input").value =  document.getElementById("input").value + "2"
+}
+function three(){
+    document.getElementById("input").value =  document.getElementById("input").value + "3"
+}
+function four(){
+    document.getElementById("input").value =  document.getElementById("input").value + "4"
+}
+function five(){
+    document.getElementById("input").value =  document.getElementById("input").value + "5"
+}
+function six(){
+    document.getElementById("input").value =  document.getElementById("input").value + "6"
+}
+function seven(){
+    document.getElementById("input").value =  document.getElementById("input").value + "7"
+}
+function eight(){
+    document.getElementById("input").value =  document.getElementById("input").value + "8"
+}
+function nine(){
+    document.getElementById("input").value =  document.getElementById("input").value + "9"
+}
+function clearInput(){
+    document.getElementById("input").value =  ""
+}
